df5572dc8245b7a2d1319de43e0d515557c0ae65
Jordan Dains <dains.jordan2@gmail.com>
2020-09-27 17:44:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,7 +59,9 @@ def invalid_command
   puts "Please enter a valid command"
 end
 
-
+#####################################################
+# get every test to pass before coding runner below #
+#####################################################
 
 def runner
   # code runner here
